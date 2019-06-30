c0d29e86ebd596a1f6c8643d86f464af9f522852
mgarcia9921 <36608240+mgarcia9921@users.noreply.github.com>
2019-06-29 21:04:08 -0500

    second commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,18 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_theme/blocs/theme.dart';
+import 'package:flutter_theme/pages/social.dart';
 
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
+const Map<String, IconData> _tabs = {
+  'Home': Icons.home,
+  'Sermon Notes': Icons.note,
+  'Devotional': Icons.refresh,
+  'Events': Icons.calendar_today,
+  'Cell Groups': Icons.group,
+  'Social': Icons.rss_feed,
+  'Contact Us': Icons.contact_mail,
+
+};
+
 class _HomeState extends State<Home> {
   int _currentIndex = 0;
   bool _isDark = false;
 
   final List<Widget> _children = [
-    PlaceholderWidget(Colors.blue),
+    HomeScreeTopPart(),
     PlaceholderWidget(Colors.green),
     PlaceholderWidget(Colors.white)
   ];
@@ -21,47 +33,38 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My App'),
+        title: Text('My App 1'),
       ),
       drawer: buildDrawer(context),
       body: _children[_currentIndex],
-      bottomNavigationBar:  buildBottomNavigationBar() ,
+      bottomNavigationBar: buildBottomNavigationBar(),
     );
   }
 
   BottomNavigationBar buildBottomNavigationBar() {
-    return BottomNavigationBar (
-      onTap: onTabTapped ,
-      currentIndex: 0 ,
-        items: [
-          BottomNavigationBarItem (
-              icon: new Icon(
-                  Icons.home
-              ) , title: new Text(
-              'Home'
-          )
-          ) ,
-          BottomNavigationBarItem (
-              icon: new Icon(
-                  Icons.rss_feed
-              ) , title: new Text(
-              'Feed'
-          )
-          ) ,
-          BottomNavigationBarItem (
-              icon: new Icon(
-                  Icons.calendar_today
-              ) , title: new Text(
-              'Events'
-          )
-          ) ,
-        ]
-        );
+    return BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        items: buildBottomNavBarItems());
   }
 
-  Drawer buildDrawer(BuildContext context)  {
-    ThemeChanger _themeChanger = Provider.of(context);
+  List<BottomNavigationBarItem> buildBottomNavBarItems() {
+    List<BottomNavigationBarItem> navBar = new List();
 
+    _tabs.forEach((k,v) => navBar.add(
+          new BottomNavigationBarItem(
+
+              icon: new Icon(v,color: Colors.black,),
+              title: new Text(k,style: TextStyle(color: Colors.black),))));
+
+    return navBar;
+  }
+
+
+
+
+  Drawer buildDrawer(BuildContext context) {
+    ThemeChanger _themeChanger = Provider.of(context);
 
     onSwitchValueChanged(bool newVal) {
       setState(() {
@@ -88,12 +91,15 @@ class _HomeState extends State<Home> {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 Column(
+                  //  mainAxisAlignment: MainAxisAlignment.start,
+
                   children: <Widget>[Text('Dark Mode')],
                 ),
                 Column(
+                  //  mainAxisAlignment: MainAxisAlignment.end,
                   children: <Widget>[
                     Switch(
                         value: _isDark,
